08c6721c53c406722ca36e9435c58aa8aa04fb07
Mitchell Johnson <ehntoo@gmail.com>
2019-05-06 16:56:02 -0400

    Put together a quick plugin for parsing peripherals out of SVD

---
+++ __init__.py
@@ -0,0 +1,25 @@
+from binaryninja import *
+from binaryninja.types import Type
+from .svdmmap import parse
+
+def load_svd(bv):
+    svd_file = interaction.get_open_filename_input("SVD File")
+    device = parse(svd_file)
+    for p in device['peripherals'].values():
+        bv.add_auto_section(p['name'], p['base'], p['size'],
+                            SectionSemantics.ReadWriteDataSectionSemantics)
+        bv.add_auto_segment(p['base'], p['size'], 0, 0,
+                            SegmentFlag.SegmentContainsData | SegmentFlag.SegmentReadable | SegmentFlag.SegmentWritable)
+        s = Structure()
+        for r in p['registers'].values():
+            s.insert(r['offset'], Type.int(int(r['size']/8), False), r['name'])
+        struct_type = Type.structure_type(s)
+        bv.define_user_type(p['name'], struct_type)
+        bv.define_data_var(p['base'], struct_type)
+
+
+PluginCommand.register(
+    "Load SVD",
+    "Apply an SVD's memory map",
+    load_svd
+)
