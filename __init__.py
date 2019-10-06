9461d89c6e78d6c6f7ebb03d8d66d00d459d026b
Mitchell Johnson <ehntoo@gmail.com>
2019-10-06 11:10:16 -0400

    Allow for derived peripherals that are encountered before their base

--- __init__.py
+++ __init__.py
@@ -5,26 +5,32 @@ from .svdmmap import parse
 def load_svd(bv):
     svd_file = interaction.get_open_filename_input("SVD File")
     device = parse(svd_file)
-    for p in device['peripherals'].values():
+    peripherals = device['peripherals'].values()
+    base_peripherals = [p for p in peripherals if 'derives' not in p]
+    derived_peripherals = [p for p in peripherals if 'derives' in p]
+
+    def register_peripheral(p, struct_type):
         bv.add_auto_section(p['name'], p['base'], p['size'],
                             SectionSemantics.ReadWriteDataSectionSemantics)
         bv.add_auto_segment(p['base'], p['size'], 0, 0,
                             SegmentFlag.SegmentContainsData | SegmentFlag.SegmentReadable | SegmentFlag.SegmentWritable)
-
-        struct_type = None
-        if 'derives' in p:
-            struct_type = bv.get_type_by_name(device['peripherals'][p['derives']]['name'])
-        else:
-            s = Structure()
-            for r in p['registers'].values():
-                if r['size'] is None:
-                    s.insert(r['offset'], Type.int(4, False), r['name'])
-                else:
-                    s.insert(r['offset'], Type.int(int(r['size']/8), False), r['name'])
-            struct_type = Type.structure_type(s)
-            bv.define_user_type(p['name'], struct_type)
         bv.define_data_var(p['base'], struct_type)
         bv.define_auto_symbol(Symbol(SymbolType.ImportedDataSymbol, p['base'], p['name']))
+
+    for p in base_peripherals:
+        s = Structure()
+        for r in p['registers'].values():
+            if r['size'] is None:
+                s.insert(r['offset'], Type.int(4, False), r['name'])
+            else:
+                s.insert(r['offset'], Type.int(int(r['size']/8), False), r['name'])
+        struct_type = Type.structure_type(s)
+        bv.define_user_type(p['name'], struct_type)
+        register_peripheral(p, struct_type)
+
+    for p in derived_peripherals:
+        struct_type = bv.get_type_by_name(device['peripherals'][p['derives']]['name'])
+        register_peripheral(p, struct_type)
 
 
 PluginCommand.register(
